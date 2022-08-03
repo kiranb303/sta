97d96b1cba0709f51670908a28d09ca17df63fa5
kiranb303 <74082741+kiranb303@users.noreply.github.com>
2022-08-04 04:31:06 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDYdgBwCXZKP2-o9ClRbXosHhU632ABZEQ",
+    authDomain: "storytellingapp-7a672.firebaseapp.com",
+    databaseURL: "https://storytellingapp-7a672-default-rtdb.firebaseio.com",
+    projectId: "storytellingapp-7a672",
+    storageBucket: "storytellingapp-7a672.appspot.com",
+    messagingSenderId: "406606585252",
+    appId: "1:406606585252:web:c9b19c936deb36e1b87531"
+  };
